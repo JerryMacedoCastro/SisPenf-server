5a642730532816a5e325a3a38847cf8989f9b994
Jerry Macedo Castro <leom551@hotmail.com>
2021-05-17 23:16:29 -0300

    feature: configure node enviroment with typescript

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-import routes from './routes';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Hi Docker!!!");
+});
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3333');
+});
