2c98d177c1b577cca791bb02b04e59f4f2817579
Jerry Macedo Castro <leom551@hotmail.com>
2021-05-22 11:14:20 -0300

    feature: create hospital controller

---
+++ src/controllers/hospital.comtroller.ts
@@ -0,0 +1,34 @@
+import { getRepository } from 'typeorm';
+import { Request, Response } from 'express';
+import { Hospital } from '../entities/hospital.entity';
+
+export default class HospitalController {
+  public async createUser(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const { name } = request.body;
+
+    try {
+      const hospitalRepository = getRepository(Hospital);
+      const isExistingHospital = await hospitalRepository.findOne({
+        name: name,
+      });
+
+      if (isExistingHospital)
+        throw new Error(
+          `The hospital ${isExistingHospital.name} already exists!!`,
+        );
+
+      const newHospital = hospitalRepository.create({
+        name,
+        isActive: true,
+      });
+      const res = await hospitalRepository.save(newHospital);
+
+      return response.status(201).send(res);
+    } catch (error) {
+      return response.status(400).send(error.message);
+    }
+  }
+}
