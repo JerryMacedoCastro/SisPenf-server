3dc2514d88fde46f316f471aaa5fdf39a344e956
JerryMacedoCastro <leom551@hotmail.com>
2023-02-02 20:22:52 -0300

    remove aws database

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,12 +2,9 @@ import path from 'path';
 import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
-  host: process.env.DATABASE_HOST,
   port: 5432,
+  url: process.env.DATABASE_URL,
   type: 'postgres',
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
   entities: [
     path.join(__dirname, 'entities/**/*.entity.ts'),
     path.join(__dirname, '**/*.entity.js'),
